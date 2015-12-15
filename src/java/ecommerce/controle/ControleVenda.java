3a631bd48eb3824937820ac500b63dee7c1c133f
GustavoHA <gustavo.agostinho0@gmail.com>
2015-12-15 11:59:38 -0200

    Porra do boleto funcionado

--- src/java/ecommerce/controle/ControleVenda.java
+++ src/java/ecommerce/controle/ControleVenda.java
@@ -19,6 +19,7 @@ import ecommerce.util.Protocolo;
 import ecommerce.util.SessionContext;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -30,6 +31,7 @@ import java.util.Date;
 import javax.faces.context.ExternalContext;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
+import org.primefaces.model.DefaultStreamedContent;
 import org.primefaces.model.StreamedContent;
 
 /**
@@ -106,6 +108,10 @@ public class ControleVenda {
 
     public void setVendaCartao(boolean vendaCartao) {
         this.vendaCartao = vendaCartao;
+    }
+
+    public StreamedContent getFile() {
+        return file;
     }
 
     public String redirecionaPaginaVenda() {
@@ -238,9 +244,18 @@ public class ControleVenda {
             fos.write(bPDF);
             fos.close();
             caminhoBoleto = nome + ".pdf";
+            carregaArquivoDownload();
         } catch (Exception ex) {
             System.out.println("Erro ao gravar Boleto MSG " + ex.getMessage());
         }
+    }
+
+    /**
+     * Metodo resposanvel por disponibilizar o arquivo para download
+     */
+    public void carregaArquivoDownload() {
+        InputStream stream = ((ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext()).getResourceAsStream("/tempBoleto/" + caminhoBoleto);
+        file = new DefaultStreamedContent(stream, null, caminhoBoleto);
     }
 
     public String getRealPath() {
@@ -362,80 +377,4 @@ public class ControleVenda {
         //return null;
     }
 
-//    public static void main(String[] args) {
-//        Datas datas = Datas.novasDatas()
-//                .comDocumento(1, 5, 2008)
-//                .comProcessamento(1, 5, 2008)
-//                .comVencimento(2, 5, 2008);
-//
-//        Endereco enderecoBeneficiario = Endereco.novoEndereco()
-//                .comLogradouro("Av das Empresas, 555")
-//                .comBairro("Bairro Grande")
-//                .comCep("01234-555")
-//                .comCidade("São Paulo")
-//                .comUf("SP");
-//
-//        //Quem emite o boleto
-//        Beneficiario beneficiario = Beneficiario.novoBeneficiario()
-//                .comNomeBeneficiario("Fulano de Tal")
-//                .comAgencia("1824").comDigitoAgencia("4")
-//                .comCodigoBeneficiario("76000")
-//                .comDigitoCodigoBeneficiario("5")
-//                .comNumeroConvenio("1207113")
-//                .comCarteira("18")
-//                .comEndereco(enderecoBeneficiario)
-//                .comNossoNumero("9000206");
-//
-//        Endereco enderecoPagador = Endereco.novoEndereco()
-//                .comLogradouro("Av dos testes, 111 apto 333")
-//                .comBairro("Bairro Teste")
-//                .comCep("01234-111")
-//                .comCidade("São Paulo")
-//                .comUf("SP");
-//
-//        //Quem paga o boleto
-//        Pagador pagador = Pagador.novoPagador()
-//                .comNome("PORRAAAAAAAAA")
-//                .comDocumento("111.222.333-12")
-//                .comEndereco(enderecoPagador);
-//
-//        Banco banco = new BancoDoBrasil();
-//
-//        Boleto boleto = Boleto.novoBoleto()
-//                .comBanco(banco)
-//                .comDatas(datas)
-//                .comBeneficiario(beneficiario)
-//                .comPagador(pagador)
-//                .comValorBoleto("200.00")
-//                .comNumeroDoDocumento("1234")
-//                .comInstrucoes("instrucao 1", "instrucao 2", "instrucao 3", "instrucao 4", "instrucao 5")
-//                .comLocaisDePagamento("local 1", "local 2");
-//
-//       //GeradorDeBoleto gerador = new GeradorDeBoleto(boleto);
-//        GeradorDeBoletoHTML gerador = new GeradorDeBoletoHTML(boleto);
-//        gerador.geraPDF("BancoDoBrasil.pdf");
-//        // Para gerar um boleto em PDF  
-//      //  gerador.geraPDF("BancoDoBrasil.pdf");
-//
-//        // Para gerar um boleto em PNG  
-//        gerador.geraPNG("BancoDoBrasil.png");
-//
-//        // Para gerar um array de bytes a partir de um PDF  
-//        byte[] bPDF = gerador.geraPDF();
-//
-//        // Para gerar um array de bytes a partir de um PNG  
-//        byte[] bPNG = gerador.geraPNG();
-//
-//        try {
-//            // File f = new File();
-//            FileOutputStream fos = new FileOutputStream("C:\\Users\\Gustavo\\Desktop\\arq\\BancoDoBrasil.pdf");
-//            fos.write(bPDF);
-//            fos.close();
-////            out.write(bPDF);
-////            out.close();
-//        } catch (IOException ex) {
-////            Logger.getLogger(Boletoto.class.getName()).log(Level.SEVERE, null, ex);
-//            System.out.println("Erro ao gravar Boleto MSG " + ex.getMessage());
-//        }
-//    }
 }
